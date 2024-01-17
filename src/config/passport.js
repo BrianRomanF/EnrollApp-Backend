5251db9d9f64d0f00029a8e79862d3f7ed80639d
BrianRomanF <broman.89@gmail.com>
2024-01-17 18:49:45 +1300

    change url

--- src/config/passport.js
+++ src/config/passport.js
@@ -7,7 +7,8 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/auth/google/callback'
+    callbackURL: 'https://enroll-backend.onrender.com/auth/google/callback',
+    passReqToCallback: true, 
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
